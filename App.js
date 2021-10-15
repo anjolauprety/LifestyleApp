f66beccc885f64b19fe0deac8ec31d0561272647
anjolauprety <89800570+anjolauprety@users.noreply.github.com>
2021-10-15 05:19:17 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {View} from 'react-native';
 import LoginPage from './components/LoginPage'
 import CreativeAssignment1 from './components/CreativeAssignment1'
 import About from './components/About'
+import Profile from './components/Profile'
 
 export default function App() {
   return (
