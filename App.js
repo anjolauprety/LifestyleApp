b1b8f1486ca4634311840adb07d3713a373447e0
anjolauprety <89800570+anjolauprety@users.noreply.github.com>
2021-11-12 10:58:12 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import {View} from 'react-native';
-
+import Recipe from './components/Recipe'
 import LoginPage from './components/LoginPage'
 import CreativeAssignment1 from './components/CreativeAssignment1'
-import About from './components/About'
 import Profile from './components/Profile'
+import About from './components/About'
 
 export default function App() {
   return (
 
-
-    <CreativeAssignment1/>
+<CreativeAssignment1/>
 
   );
 }
