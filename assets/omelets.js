56f367a74930b70e3a8bb378a676966398f217d8
anjolauprety <89800570+anjolauprety@users.noreply.github.com>
2021-12-03 11:05:03 -0500

    Add files via upload

---
+++ assets/omelets.js
@@ -0,0 +1,65 @@
+export default function Omelets(){
+  return (
+[
+{
+"title": "Porridge with Stewed Apple",
+"href": "http://find.myrecipes.com/recipes/recipefinder.dyn?action=displayRecipe&recipe_id=520763",
+"ingredients": "1 medium apple, 5.1fl oz water, ground cinnamon,1pinch sea salt, 1.1oz rolled oats,8.5fl oz calcium-fortified non-diary milk, 3.5oz calcium-fortified non-diary yogurt",
+
+},
+{
+"title": "Banana & Almonds with Yoghurt",
+"href": "http://www.bigoven.com/43919-Roasted-Pepper-and-Bacon-Omelet-recipe.html",
+"ingredients": "eggs, salt, black pepper, butter, black pepper, bacon, onions, garlic, roasted red peppers, oregano, black pepper",
+
+},
+{
+"title": "Salad & Bean Wrap",
+"href": "http://cookeatshare.com/recipes/broccoli-oven-omelet-92851",
+"ingredients": "eggs, broccoli, onions, parmesan cheese, lowfat milk, salt, basil, garlic, tomato, parmesan cheese",
+
+},
+{
+"title": "Crispbreads with Avocado & Rocket",
+"href": "http://www.epicurious.com/recipes/food/views/Eggplant-Omelet-with-Coriander-and-Caraway-306",
+"ingredients": "caraway seed, coriander, eggplant, eggs, garlic, lemon, olive oil, onions, black pepper, salt",
+
+},
+{
+"title": "Sweet Potato & Black Bean Enchilada",
+"href": "http://www.bigoven.com/143831-Eggplant-Omelet-with-Coriander-And-Caraway-recipe.html",
+"ingredients": "eggplant, olive oil, onions, garlic, egg, caraway seed, coriander, salt, black pepper, lemon",
+
+},
+{
+"title": "Blueberry & Lemon Breakfast Quinoa",
+"href": "http://www.bigoven.com/45151-Broccoli-and-Cheese-Omelet-recipe.html",
+"ingredients": "4.2fl oz water, 8.5fl oz calcium-fortified non-diary milk, 2.1oz quinoa flakes, lemon, 2 tsp pure maple syrup",
+
+},
+{
+"title": "Hardboiled Egg & Tomato on Toast",
+"href": "http://www.bigoven.com/149383-Chard-And-Onion-Omelet-recipe.html",
+"ingredients": "1 large egg, 1 slice wholemeal bread, 1 small handful rocket leaves,1/2 medium tomato sliced,1/4 small red onion,sea salt, ground black pepper",
+
+},
+{
+"title": "Asian Noodle Salad",
+"href": "http://www.cdkitchen.com/recipes/recs/2184/Picnic-Omelet-Squares99498.shtml",
+"ingredients": "vermicelli noodles, garlic cloves, reduced soy sauce, lime, dried chilli flakes, oil spray, firm tofu, medium red capsicum, snow peas, medium carrot, spring onion, baby spinach, coriander, mint leaves",
+
+},
+{
+"title": "Passionfruit & Mango Mousse",
+"href": "http://www.cooks.com/rec/view/0,185,153160-249194,00.html",
+"ingredients": "1/2 medium mango, 10.6oz calcium-fortified non-diary yoghurt, 3 passionfruit",
+
+},
+{
+"title": "Vegetarian Paella",
+"href": "http://find.myrecipes.com/recipes/recipefinder.dyn?action=displayRecipe&recipe_id=1622444",
+"ingredients": "3 tbsp olive oil, 1/4 small brown onion,1/2 garlic clove,1/4tsp ground turmeric, 2.1oz brown rice, 1/4 medium red capsicum,1/2 medium tomato diced, 11.8fl oz salt-reduced vegetable stock, 0.7oz frozen peas, 0.7oz frozen corn kernels, 5.3oz tinned chickpeas, 1 tbsp fresh prasley, sea salt, ground black pepper, lemon",
+
+}
+]
+)}
